6eba5f6e99c8efd8ffe338ecc0211eb4f0b58a5d
madhi-avs <madhis@avatarsoft.ca>
2021-10-02 14:23:57 +0530

    Added test data for the RoomBookingDetailsService

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,7 +16,11 @@ extra_child DECIMAL(20,2)
 commit;
 
 INSERT INTO roombookingdetails(room_id,max_adults,max_children,max_child_age,from_date,to_date,base_room_price,extra_adult,extra_child) VALUES
-('R123',3,2,12,PARSEDATETIME('01-SEP-2021' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2021' , 'dd-MMM-yy' ,  'en'),1000,500,200);
+('R123',3,2,12,PARSEDATETIME('01-SEP-2021' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2021' , 'dd-MMM-yy' ,  'en'),1000,500,200),
+('R123',3,2,12,PARSEDATETIME('01-FEB-2022' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2022' , 'dd-MMM-yy' ,  'en'),2000,700,300),
+('R124',3,2,12,PARSEDATETIME('01-SEP-2021' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2021' , 'dd-MMM-yy' ,  'en'),2000,700,400),
+('R124',3,2,12,PARSEDATETIME('01-FEB-2022' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2022' , 'dd-MMM-yy' ,  'en'),3000,900,600)
+;
 
 commit;
 select * from roombookingdetails;
