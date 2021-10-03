14404a0a90b97556df2375bc02ad355ff2aa7c40
madhi-avs <madhis@avatarsoft.ca>
2021-10-03 17:05:56 +0530

    Added required schema and insertted test data  for exercise 2.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,3 +21,56 @@ INSERT INTO roombookingdetails(room_id,max_adults,max_children,max_child_age,fro
 ('R124',3,2,12,PARSEDATETIME('01-FEB-2022' , 'dd-MMM-yy' ,  'en'),PARSEDATETIME('31-DEC-2022' , 'dd-MMM-yy' ,  'en'),3000,900,600)
 ;
 commit;
+
+DROP TABLE IF EXISTS userdetails;
+commit;
+CREATE TABLE  userdetails (
+user_id INT  PRIMARY KEY,
+tier VARCHAR(250),
+coins INT,
+referrer_id INT,
+total_amount_spent INT,
+);
+commit;
+
+Insert Into userdetails (user_id,tier,coins,referrer_id,total_amount_spent) VALUES (1,'Gold',0,0,1000),
+(21,'Gold',0,0,1000),
+(22,'Gold',0,0,1000),
+(23,'Gold',0,0,1000),
+(24,'Gold',0,0,1000);
+commit;
+
+DROP TABLE IF EXISTS bookings;
+commit;
+CREATE TABLE  bookings (
+trip_id INT AUTO_INCREMENT  PRIMARY KEY,
+user_id INT ,
+start_date DATE,
+end_date DATE,
+referrer_id INT,
+trip_type VARCHAR(250),
+trip_status VARCHAR(250),
+amount_spent INT,
+is_referral_booking INT
+);
+commit;
+DROP TABLE IF EXISTS coins;
+commit;
+CREATE TABLE  coins (
+tier VARCHAR(250),
+trip_type VARCHAR(250),
+coins_value INT
+);
+commit;
+Insert Into coins (tier,trip_type,coins_value) VALUES
+('Silver','Domestic',500),
+('Silver','International',1000),
+('Gold','Domestic',1000),
+('Gold','International',2000),
+('Platinum','Domestic',2000),
+('Platinum','International',4000);
+commit;
+
+
+
+
