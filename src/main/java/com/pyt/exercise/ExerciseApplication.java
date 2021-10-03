3e504e63f7da3054c57c87cb0605ebfdee388f9b
madhi-avs <madhis@avatarsoft.ca>
2021-10-03 17:09:01 +0530

    Enabled Scheduling for application

--- src/main/java/com/pyt/exercise/ExerciseApplication.java
+++ src/main/java/com/pyt/exercise/ExerciseApplication.java
@@ -5,9 +5,11 @@ package com.pyt.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
